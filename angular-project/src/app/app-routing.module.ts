14145eb4c04ecede52c17265dbbb2686d7b7b1d6
AdelfaPoesis <AdelfaPoesis@gmail.com>
2019-04-12 15:06:09 +0300

    add table, change scss

--- angular-project/src/app/app-routing.module.ts
+++ angular-project/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ListTableComponent } from './list-table/list-table.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: ListTableComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- angular-project/src/app/app.module.ts
+++ angular-project/src/app/app.module.ts
@@ -3,14 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponentComponent } from './header-component/header-component.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ListTableComponent } from './list-table/list-table.component';
+import { MatTableModule } from '@angular/material/table';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponentComponent,
+    ListTableComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatTableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
